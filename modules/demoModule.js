30fb2815bdb047f38987ded5c11617f45df161ed
Vermel, Runn (GE Global Research) <runn.vermel@ge.com>
2016-11-01 14:16:58 -0700

    demoModule docs done

--- modules/demoModule.js
+++ modules/demoModule.js
@@ -5,31 +5,46 @@ var Promise = require('bluebird'),
     fileExists = require('file-exists');
 
 
+/**
+ * our IIFE function
+ */
 var updateDependency = (function() {
 
-  var change = function(dir) {
-    console.log(dir + '/bower.json');
-
+  /**
+   * changes the Dependency for polymer to 1.7.0 for the given repo/dir
+   * @param  {[String]} dir [a full path to a repo]
+   * @return {[Promise]}     [a resolved promise]
+   */
+  var changeDependencyVersion = function(dir) {
+    //check if bower exists, and if not, resolve the promise without doing anything.
     let doesFileExist = fileExists(dir + '/bower.json');
     if (!doesFileExist) return Promise.resolve(dir);
 
     return fs.readJsonAsync(dir + '/bower.json')
       .then((bower) => {
-        console.log('have bower' + dir);
+        //make sure bower has dependencies and a polymer parameter.
         if (bower.dependencies && bower.dependencies.polymer) {
-          console.log('have polymer');
           bower.dependencies.polymer = "^1.7.0";
         }
+        //don't forget to return the resolved promise - which is picked up next
         return Promise.resolve(bower);
       })
+      //resolved promise is picked up here
       .then((bower) => {
-        console.log('write json ' + dir);
+        //and bower is async written back. by promisifying everything inside the fs-extra lib, we ensure we get a promise back our of any fs method.
         return fs.writeJsonAsync(dir + '/bower.json', bower);
       });
   };
 
+  /**
+   * our Main function. calls the changeDependencyVersion function, and once that's done, calls the callback (cb), which doesn't actually do anything
+   * but is needed for this module to be promisified.
+   * @param  {[String]}   dir [a full path to a repo]
+   * @param  {Function} cb  [a dummy callback, needed to promisify this module]
+   * @return {[Promise]}       [returns a resolved promise.]
+   */
   var main = function(dir, cb) {
-    change(dir).then((r) => {
+    changeDependencyVersion(dir).then((r) => {
       cb(null,'this is what returns');
     });
   };
