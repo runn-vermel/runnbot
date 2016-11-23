587b03ed5f0782022e6192ce2c6c88fa5ac15025
RunnBot <runnbot@ge.com>
2016-11-22 17:15:08 -0800

    update px-theme to 2.0.1 and update the test fixtures

--- modules/replaceStringInFile.js
+++ modules/replaceStringInFile.js
@@ -17,9 +17,45 @@ var changeTextInFile = (function() {
   var removeTextFromFile = function(dir) {
     var includeText = '<style include="px-theme-styles"></style>',
         regEx = new RegExp(includeText, "g");
+
+    var isFileOrDir = function(firOrDir) {
+
+    };
+
+
+
+
+    return fs.readdirAsync(reposDir)
+        // Only return files that are a directory
+        .filter((fileName) => fs.statAsync(`${reposDir}/${fileName}`).then((stat) => stat.isDirectory()) )
+        .filter((fileName) => {
+          // Only return files that are in the whitelist, if there is a whitelist
+          if (whitelist.length) return Promise.resolve(whitelist.indexOf(fileName) !== -1);
+          // If no whitelist, return all files
+          return Promise.resolve(true);
+        })
+        .then((repos) => {
+          debugger;
+          if (repos.length) return Promise.resolve(repos);
+          return Promise.reject(new Error(`No repos found in directory ${chalk.bold(reposDir)}. Please make sure you are running the search in the correct directory.`))
+        });
+
+
+
+
+
+
+
+
+
+
+
+
     return fs.readdirAsync(dir)
-    .then((files) => files.filter((file) => (file !== "index.html" && file !=="demo.html")))
-    .then((files) => files.filter((fileOrDir) => fileOrDir.substr(0, 1) !== "." && fs.statSync(dir + "/" + fileOrDir).isFile()))
+    .filter((file) => (file !== "index.html" && file !=="demo.html"))
+    .filter((fileOrDir) => fileOrDir.substr(0, 1) !== ".")gits
+
+    .filter((filename) => fs.statAsync(`${dir}/${filename}`).then((stat) => stat.isFile()) )
     .then((files) => {
 
     return Promise.each(files, (file) => {
@@ -28,7 +64,7 @@ var changeTextInFile = (function() {
 
                 if (fileText.indexOf(includeText) > -1) {
                   var updatedText = fileText.replace(regEx, '');
-                  fs.writeFileAsync(dir + "/" + file, updatedText)
+                  fs.writeFileAsync(dir + "/" + file, updatedText, {spaces: 2})
                   .then(() => Promise.resolve());
                 } else {
                   return Promise.resolve();
---
+++ modules/updatePxTheme.js
@@ -0,0 +1,102 @@
+
+var Promise = require('bluebird'),
+    shared = require('../lib/shared'),
+    fs = Promise.promisifyAll(require('fs-extra'));
+    fileExists = require('file-exists');
+
+
+/**
+ * our IIFE function
+ */
+var changeTextInFile = (function() {
+
+  /**
+   * removes the specified text from a file.
+   * @param  {[String]} dir [a full path to a repo]
+   * @return {[Promise]}     [a resolved promise]
+   */
+  var removeTextFromFile = function(dir) {
+    var initialDir = dir;
+    var searchText = `<link rel="import" href="../../px-theme/px-theme.html"/>`,
+        regEx = new RegExp(searchText, "g"),
+        replaceText = `<link rel="import" href="../../px-theme/px-theme-styles.html">
+    <style include="px-theme-styles" is="custom-style"></style>`;
+
+    return fs.readdirAsync(dir + "/test")
+    .then((files) => files.filter((file) => (file.substring(file.length-12) == "fixture.html")))
+    //.then((files) => files.filter((fileOrDir) => fileOrDir.substr(0, 1) !== "." && fs.statSync(dir + "/" + fileOrDir).isFile()))
+    .then((files) => {
+
+    return Promise.each(files, (file) => {
+      return fs.readFileAsync(dir + "/test/" + file, "utf-8")
+               .then((fileText) => {
+                if (fileText.indexOf(searchText) > -1) {
+                  var updatedText = fileText.replace(regEx, replaceText);
+                  fs.writeFileAsync(dir + "/test/" + file, updatedText, {spaces: 2})
+                  .then(() => Promise.resolve(initialDir));
+                } else {
+                  return Promise.resolve(initialDir);
+                }
+              });
+    });
+  });
+  };
+
+  var changeDeps = function(dir) {
+    let doesFileExist = fileExists(dir + '/bower.json');
+    if (!doesFileExist) return Promise.resolve(dir);
+
+    return fs.readJsonAsync(dir + '/bower.json')
+      .then((bower) => {
+        //make sure bower has dependencies and a polymer parameter.
+        // if your condition is met, resolve the promise with 2 parameters: bower, and changed.
+        if ((bower.devDependencies && bower.devDependencies["px-theme"]) || (bower.dependencies && bower.dependencies["px-theme"])) {
+           if (bower.devDependencies["px-theme"]) {
+             bower.devDependencies["px-theme"] = "^2.0.1";
+           }
+
+           if (bower.dependencies["px-theme"]) {
+                    bower.dependencies["px-theme"] = "^2.0.1";
+           }
+
+          return Promise.resolve({bower:bower, changed: true});
+        } else {
+          return Promise.resolve({bower:bower, changed: false});
+        }
+        //don't forget to return the resolved promise - which is picked up next
+
+      })
+      //resolved promise is picked up here
+      .then((obj) => {
+        //and bower is async written back if changed is true. by promisifying everything inside the fs-extra lib, we ensure we get a promise back our of any fs method.
+        if (obj.changed) {
+          return fs.writeJsonAsync(dir + '/bower.json', obj.bower, {spaces: 2})
+                    .then(() => Promise.resolve(dir));
+        } else {
+          return Promise.resolve(dir);
+        }
+      });
+  };
+
+  /**
+   * our Main function. calls the removeTextFromFile function, and once that's done, calls the callback (cb), which doesn't actually do anything
+   * but is needed for this module to be promisified.
+   * @param  {[String]}   dir [a full path to a repo]
+   * @param  {Function} cb  [a dummy callback, needed to promisify this module]
+   * @return {[Promise]}       [returns a resolved promise.]
+   */
+  var main = function(dir, cb) {
+    changeDeps(dir)
+    .then((dir) => removeTextFromFile(dir))
+    .then((r) => {
+      cb(null,'this is what returns');
+    });
+  };
+  return {
+    main : main
+  };
+})();
+
+module.exports = {
+  main: changeTextInFile.main
+};
